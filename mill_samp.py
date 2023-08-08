4578ef0c1624e9b6a96f6999253200d0262b4744
Mihauk <49325524+Mihauk@users.noreply.github.com>
2023-08-08 14:22:21 -0400

    Update mill_samp.py

--- mill_samp.py
+++ mill_samp.py
@@ -44,35 +44,48 @@ class conf:
         self.ft = np.fft.ifft(self.state)
         #self.cur_ft = np.fft.ifft(self.cur)
 
-n = 8
+def realization():
+    x = conf(ini_conf,n)
+    #m_rho[:,0] = copy.deepcopy(x.state)
+    m_rho_ft[:,0] += copy.deepcopy(x.ft)
+    #qb2[0] = copy.deepcopy(x.ft[15])**2
+    for i in range(t):
+        x.update(n,np.random.randint(3))
+        #qb2[i+1] = copy.deepcopy(x.ft[15])**2
+        #m_rho[:,i+1] = copy.deepcopy(x.state)
+        m_rho_ft[:,i+1] += copy.deepcopy(x.ft)
+        #m_j[:,i] = copy.deepcopy(x.cur)
+        #m_j_ft[:,i] = copy.deepcopy(x.cur_ft)
+    m_rho_ft_sq = m_rho_ft*m_rho_ft
+    avg = m_rho_ft/c
+    var = (m_rho_ft_sq/c) -  avg*avg
+    return avg, var
+
+
+n = 100
 l = 3*n
 t = 100
 A = 0.1
-samples =50000000
+samples = 1000000
 freq = 2*np.pi*np.fft.fftfreq(l)
 Q = freq[2]
 #m_rho = np.zeros((l,t+1))
 #m_j = np.zeros((l,t))
 m_rho_ft = np.zeros((l,t+1),dtype='complex')
+m_rho_ft_sq = np.zeros((l,t+1),dtype='complex')
 #m_j_ft = np.zeros((l,t),dtype='complex')
 #qb2 = np.zeros(t+1,dtype='complex')
 ini_conf = A*np.cos(Q*np.arange(l))+0.5
 #ini_conf = (0.02*np.cos(freq[20]*np.arange(l)) + 0.02*np.cos(freq[40]*np.arange(l)))+0.6
 
+c = 0
+m, var = []
 for j in range(samples):
-    x = conf(ini_conf,n)
-    #m_rho[:,0] = copy.deepcopy(x.state) + m_rho[:,0]
-    m_rho_ft[:,0] = copy.deepcopy(x.ft) + m_rho_ft[:,0]
-    #qb2[0] = copy.deepcopy(x.ft[15])**2 + qb2[0]
-    for i in range(t):
-        x.update(n,np.random.randint(3))
-        #qb2[i+1] = copy.deepcopy(x.ft[15])**2 + qb2[i+1]
-        #m_rho[:,i+1] = copy.deepcopy(x.state) + m_rho[:,i+1]
-        m_rho_ft[:,i+1] = copy.deepcopy(x.ft) + m_rho_ft[:,i+1]
-        #m_j[:,i] = copy.deepcopy(x.cur) + m_j[:,i]
-        #m_j_ft[:,i] = copy.deepcopy(x.cur_ft) + m_j_ft[:,i]
+    c += 1
+    m, var = realization()
+    
 
 with open(f'../data/fms_0.5_A{A}_t{t}_k2_L{l}.pickle', 'wb') as data:
         #pickle.dump(qb2/samples,data)
 	#pickle.dump([m_rho/samples, m_rho_ft/samples, m_j/samples, m_j_ft/samples], data)
-	pickle.dump(m_rho_ft[2]/samples, data)
+	pickle.dump([m, var], data)

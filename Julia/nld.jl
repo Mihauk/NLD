dc002c72daa082d8796694a5bf267ab04cced0fb
Abhishek Raj <49325524+Mihauk@users.noreply.github.com>
2024-01-30 15:19:32 -0500

    Add files via upload

--- Julia/nld.jl
+++ Julia/nld.jl
@@ -1,8 +1,12 @@
 using FFTW
 using Plots
+#using Folds
 using Random
 using Statistics
+#using Distributed
+#addprocs(8)
 using BenchmarkTools
+
 
 function trimer_updates(trip)
     if trip == [0, 1, 0]
@@ -15,31 +19,38 @@ function trimer_updates(trip)
     return convert(Vector{Bool},trip)
 end
 
-function update!(state, off)
-    state = circshift(collect(Iterators.flatten(map(trimer_updates, Iterators.partition(circshift(state, -off), 3)))), off)
+function update(state, off)
+    #return circshift(collect(Iterators.flatten(map(trimer_updates, Iterators.partition(circshift(state, -off), 3)))), off)
+    return circshift(collect(reduce(vcat,map(trimer_updates, Iterators.partition(circshift(state, -off), 3)))), off)
 end
 
 
-l = 999999
-n0 = 0.5
-A = 0.1
-q = 30
-tmax = 10
-freq = 2π * fftfreq(l)
-Q = freq[q]
-rho_in = n0 .+ A * cos.(Q * collect(1:l))
-samples = 100
-
-
-state = rand(Float64, l) .<= rho_in
-a = zeros(Bool, samples, l, tmax+1)
-
-@time begin
+function my_func(samples, tmax, l)
+    n0 = 0.5
+    A = 0.1
+    q = 6
+    freq = 2π * rfftfreq(l)
+    Q = freq[q]
+    rho_in = n0 .+ A * cos.(Q * collect(1:l))
+    a = zeros(Bool, samples, l, tmax+1)
     for i in 1:samples
-        off = rand(0:2)
-        a[i,:,1] = rand(Float64, l) .<= rho_in
-        for t in collect(1:tmax)
-            a[i,:,t+1] = update!(state, off)
+        state = rand(Float64, l) .<= rho_in
+        a[i,:,1] = state
+        for t in 1:tmax
+            state = update(state, rand(0:2))
+            a[i,:,t+1] = state
         end
     end
+    return a
 end
+
+trange = 10 * range(1, length=10)
+srange = range(10, length=10, step=10)
+lrange = 99 * range(1,stop=3)
+
+x = [[[(@elapsed my_func(sr,tr,lr) )/(lr*tr*sr) for sr in srange] for tr in trange] for lr in lrange]
+
+plot(srange, [x[3][i] for i in 1:length(trange)], label=[10 20 30 40 50 60 70 80 90 100])
+xlabel!("samples")
+ylabel!("time")
+savefig("benchmark_jl.png")

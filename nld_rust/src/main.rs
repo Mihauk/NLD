1e1393b26e0c70fb1667d0242812679c1cd5e9a2
Abhishek Raj <49325524+Mihauk@users.noreply.github.com>
2024-08-01 15:38:58 -0400

    Update main.rs

--- nld_rust/src/main.rs
+++ nld_rust/src/main.rs
@@ -1,5 +1,7 @@
 use nld::with_threads;
 use clap::Parser;
+use hdf5::File;
+//use ndarray::Array1;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -11,6 +13,9 @@ struct Args {
     /// Total number of samples
     #[arg(short='s', long="samples", default_value_t = 100)]
     samples: usize,
+
+    #[arg(short='S', long="outer_samples", default_value_t = 100)]
+    outer_samples: usize,
 
     /// Total number of time steps
     #[arg(short='t', long="t_max", default_value_t = 100)]
@@ -35,10 +40,78 @@ fn main() {
 
     let args = Args::parse();
 
-    let result = with_threads(args.samples, args.t_max, args.chain_length, args.wave_number, args.amplitude, args.eq_den);
+    println!("Start!");
+    println!("Running 1D non-linear simulation with the following parameters:");
+    println!("{:?}", args);
 
-    println!("{:?}", result.m_rho);
-    println!("{:?}", result.v_rho);
-    println!("{:?}", result.s_rho);
-    println!("{:?}", result.k_rho);
+    let n0 = args.eq_den;
+    let a = args.amplitude;
+    let l = args.chain_length;
+    let q = args.wave_number;
+    let tmax = args.t_max;
+    let samples = args.samples;
+    let t_samples = args.outer_samples;
+
+    let filename = format!("./target/data/rho_dotp-n_{}-A_{}-q_{}-t_samples_{}-samples_each_run_{}-tmax_{}-l-{}.h5",
+                           n0, a, q, t_samples, samples, tmax, l);
+    let dataset_names = ["m1", "m2", "m3", "m4"];
+
+    let data = with_threads(samples, tmax, l, q, a, n0);
+
+
+    let file = File::create(filename).unwrap();
+
+    for (i, &name) in dataset_names.iter().enumerate() {
+        // Create a new dataset
+        let dataset = file.new_dataset::<f64>().shape(data[i].len()).create(name).unwrap();
+        // Write the combined data to the dataset
+        dataset.write(&data[i]).unwrap();
+    }
+
+
+ /*   
+    for outer in 0..=t_samples {
+        let data= with_threads(samples, tmax, l, q, a, n0);
+
+        let file = File::open_rw(&filename).unwrap_or_else(|_| File::create(&filename).unwrap());
+
+        for (i, &dataset_name) in dataset_names.iter().enumerate() {
+            let dataset = file.new_dataset::<f64>().shape(data[i].len()).create(dataset_name).unwrap();
+            if outer == 0 {
+                dataset.write(&data[i]).unwrap();
+            } else {
+                let mut previous_data = dataset.read().unwrap();
+                for (j, &value) in data[i].iter().enumerate() {
+                    previous_data[j] += value;
+                }
+                dataset.write(&previous_data).unwrap();
+            }
+        }
+    }
+
+    for outer in 0..=t_samples {
+        // Initialize or read previous data
+        let previous_data: [Vec<f64>] = if outer == 0 {
+            vec![Vec<f64>::zeros(tmax); 4]
+        } else {
+            let file = File::open(filename).unwrap();
+            dataset_names.iter().map(|&name| {
+                let dataset = file.dataset(name).unwrap();
+                dataset.read_1d::<f64>().unwrap()
+            }).collect()
+        };
+
+        let data: [Vec<f64>; 4] = with_threads(samples, tmax, l, q, a, n0); // your computations here
+
+        // Save the data at the end of the inner loop
+        let file = File::create(filename).unwrap();
+        for (i, &name) in dataset_names.iter().enumerate() {
+            let dataset = file.new_dataset::<f64>().create(name).unwrap();
+            let combined_data = &previous_data[i] + &data[i];
+            dataset.write(&combined_data).unwrap();
+        }
+    }
+ */
+
+    println!("Done!");
 }

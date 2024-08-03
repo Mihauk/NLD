fe3c5fc39fb1bc6c58f600b903cef99da07ce765
Abhishek Raj <49325524+Mihauk@users.noreply.github.com>
2024-08-02 20:33:46 -0400

    Update lib.rs

--- nld_rust/src/lib.rs
+++ nld_rust/src/lib.rs
@@ -2,6 +2,7 @@ use rand::Rng;
 //use rustfft::num_complex::Complex;
 //use rustfft::FftPlanner;
 use std::f64::consts::PI;
+use rayon::prelude::*;
 //use ndarray::arr2;
 
 #[derive(Debug)]
@@ -21,6 +22,10 @@ fn rfftfreq(n: usize) -> Vec<f64> {
     (0..n / 2 + 1).map(|i: usize| i as f64 / n as f64).collect()
     }
 
+//##################################################################################################################################################
+    
+/// Update the configuration of the system sequentially
+/* 
 fn update(config: &mut Conf, p: &[bool]) {
     for (j, &pj) in p.iter().enumerate() {
         let idx: usize = 3 * j;
@@ -39,12 +44,42 @@ fn update(config: &mut Conf, p: &[bool]) {
         }
     }
 }
+*/
+
+//##################################################################################################################################################
+
+/// Update the configuration of the system in parallel
+fn update(config: &mut Conf, p: &[bool]) {
+    config.st.par_chunks_mut(3).zip(p.par_iter()).for_each(|(chunk, &pj)| {
+        match chunk {
+            [true, false, false] => {
+                chunk[0] = false;
+                chunk[1] = pj;
+                chunk[2] = !pj;
+            }
+            [false, true, false] => {
+                chunk[0] = pj;
+                chunk[1] = false;
+                chunk[2] = !pj;
+            }
+            [false, false, true] => {
+                chunk[0] = pj;
+                chunk[1] = !pj;
+                chunk[2] = false;
+            }
+            _ => {}
+        }
+    });
+}
+
+//##################################################################################################################################################
+
 
 pub fn with_threads(samples: usize, tmax: usize, l: usize, q: usize, a: f64, n0: f64) -> [Vec<f64>; 4] {
     let freq: Vec<f64> = rfftfreq(l);
     let q_freq: f64 = freq[q+1];
-    let ax: Vec<f64> = (0..l).map(|i: usize| (q_freq * i as f64 * 2.0 * PI).cos()).collect();
-    let rho_in: Vec<f64> = ax.iter().map(|&x| n0 + a * x).collect();
+    let ax: Vec<f64> = (0..l).into_par_iter().map(|i: usize| (q_freq * i as f64 * 2.0 * PI).cos()).collect();
+    let rho_in: Vec<f64> = ax.par_iter().map(|&x| n0 + a * x).collect();
 
     let mut my_data: TotalData = TotalData {
         m1: vec![0.0; tmax],
@@ -60,8 +95,8 @@ pub fn with_threads(samples: usize, tmax: usize, l: usize, q: usize, a: f64, n0:
         };
 
         //let rho_0: f64 = config.st.iter().zip(&ax.iter()).map(|(&s, &ax)| if s { a } else { 0.0 }).sum::<f64>() / l as f64;
-        let dot_p: f64 = config.st.iter()
-            .zip(ax.iter())
+        let dot_p: f64 = config.st.par_iter()
+            .zip(ax.par_iter())
             .map(|(&st, &ax)| if st { ax } else { 0.0 })
             .sum();
 
@@ -80,8 +115,8 @@ pub fn with_threads(samples: usize, tmax: usize, l: usize, q: usize, a: f64, n0:
             update(&mut config, &p);
             config.st.rotate_left(off);
             //let rho_t: f64 = config.st.iter().zip(&ax.iter()).map(|(&s, &ax)| if s { a } else { 0.0 }).sum::<f64>() / l as f64;
-            let dot_p_t: f64 = config.st.iter()
-                .zip(ax.iter())
+            let dot_p_t: f64 = config.st.par_iter()
+                .zip(ax.par_iter())
                 .map(|(&st, &ax)| if st { ax } else { 0.0 })
                 .sum();
             let rho_t: f64 = dot_p_t / l as f64;

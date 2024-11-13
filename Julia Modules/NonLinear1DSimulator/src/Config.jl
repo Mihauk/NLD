bc3a24e8eaa8f05b67f0ae2269ec88079049c923
Abhishek Raj <49325524+Mihauk@users.noreply.github.com>
2024-11-13 13:49:55 -0500

    Update Config.jl

--- Julia Modules/NonLinear1DSimulator/src/Config.jl
+++ Julia Modules/NonLinear1DSimulator/src/Config.jl
@@ -7,6 +7,7 @@ using Random
 # Structure representing the configuration state
 mutable struct Conf
     st::BitVector
+    curr::Union{Vector{Int}, Nothing}  # Optional current field
 end
 
 # Structure for storing simulation data
@@ -18,13 +19,15 @@ mutable struct TotalData
 end
 
 """
-    initQ(L::Int, nQ::Int, n0::Float64, A::Float64) -> BitVector
+    initQ(L::Int, nQ::Int, n0::Float64, A::Float64, compute_current::Bool) -> Conf
 
-Initialize the configuration `st` as a BitVector of length `L`, where each bit represents the presence (1) or absence (0) of a particle. The initial configuration is perturbed from the equilibrium density `n0` with amplitude `A` and wave number `nQ`.
+Initialize the configuration `st` as a BitVector of length `L`, where each bit represents the presence (1) or absence (0) of a particle. The initial configuration is perturbed from the equilibrium density `n0` with amplitude `A` and wave number `nQ`. If `compute_current` is `true`, initializes `curr` as a zero vector.
 """
-function initQ(L::Int, nQ::Int, n0::Float64, A::Float64)
+function initQ(L::Int, nQ::Int, n0::Float64, A::Float64, compute_current::Bool)
     Q = 2 * π * nQ / L
-    return [x >= 0 ? true : false for x in n0 .+ A .* cos.(Q .* (0:L-1)) .+ (rand(L) .- 1)]
+    st = [x >= 0 ? true : false for x in n0 .+ A .* cos.(Q .* (0:L-1)) .+ (rand(L) .- 1)]
+    curr = compute_current ? zeros(Int, L) : nothing
+    return Conf(st, curr)
 end
 
 end # module
